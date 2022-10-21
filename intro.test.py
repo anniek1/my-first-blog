edb7e59f985ccd70b5f84735e446138ab02d062f
Annie <anastasia.constantopoulou.arc@gmail.com>
2022-10-21 07:14:19 -0700

    My Django Girls app, first commit

---
+++ intro.test.py
@@ -0,0 +1,28 @@
+# name='anastasia'
+
+# 2>3
+# print(2>3)
+
+# print(name)
+# a=2**7
+# b=2/2
+# if a>b:
+#    print('this is fantastic')
+
+# def hi(name):
+#     print('hi '+ name + ' there!')
+#     print('we like you')
+
+# hi("Annie")
+
+# def hi(name):
+#     print('Hi ' + name + '!!')
+
+# girls=['Michaela', 'Rachel', 'Dina', 'Walmer']
+# for name in girls:
+#     hi(name)
+#     print('Next please')
+
+for i in range(1,22):
+    print(i)
+
